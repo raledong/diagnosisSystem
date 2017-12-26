76546a84e945cd47c0666f2409bf79c6d5d3c508
soleil <705452492@qq.com>
2017-12-26 11:47:10 +0800

    照片详情界面添加图片切换／回复／标签展示

--- src/main/java/cn/nju/model/Photo.java
+++ src/main/java/cn/nju/model/Photo.java
@@ -71,7 +71,7 @@ public class Photo {
     }
 
     public String[] getTids(){
-        return tid.split(",");
+        return tid==null ? new String[0] : tid.split(",");
     }
     public void setTid(String tid) {
         this.tid = tid;
